9c18787ad5d5368f3fbb324a98eb384a9b625fee
PriyeshRaj231 <rajpriyesh640@gmail.com>
2025-08-12 22:31:07 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 # exit on error
 set -o errexit
 
-# Install system dependencies for face recognition
-apt-get update
-apt-get install -y cmake build-essential libboost-all-dev
-
 # Install Python dependencies
 pip install -r requirements.txt
+
